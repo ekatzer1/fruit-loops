fcd58f0acab721da6d8680fd4851dd563bd6142b
Emmet <ekatzer@sjcawi.org>
2025-10-20 10:48:35 -0500

    Better list formatting

--- app.js
+++ app.js
@@ -3,63 +3,63 @@
 // Use them to develop code mentioned in the comments below
 
 var fruits = [
-    "apple",
-    "banana",
-    "grapes",
-    "orange",
-    "dragonfruit",
-    "kiwi",
-    "starfruit"
+    "apple"
+    ,"banana"
+    ,"grapes"
+    ,"orange"
+    ,"dragonfruit"
+    ,"kiwi"
+    ,"starfruit"
 ];
 
 var calories = [
-    95,
-    105,
-    35,
-    69,
-    61,
-    42,
-    28
+    95
+    ,105
+    ,35
+    ,69
+    ,61
+    ,42
+    ,28
 ];
 
 var carbohydrates = [
-    25,
-    27,
-    9,
-    18,
-    15,
-    10,
-    6.1
+    25
+    ,27
+    ,9
+    ,18
+    ,15
+    ,10
+    ,6.1
 ];
 
 var sugars = [
-    19,
-    14,
-    7.6,
-    12,
-    9,
-    6.2,
-    3.6
+    19
+    ,14
+    ,7.6
+    ,12
+    ,9
+    ,6.2
+    ,3.6
 ];
 
 var fat = [
-    0.3,
-    0.4,
-    0.1,
-    0.2,
-    0.5,
-    0.4,
     0.3
+    ,0.4
+    ,0.1
+    ,0.2
+    ,0.5
+    ,0.4
+    ,0.3
 ];
 
 var protein = [
-    0.5,
-    0.4,
-    0.4,
-    1.3,
-    1.1,
-    0.8,
-    1
+    0.5
+    ,0.4
+    ,0.4
+    ,1.3
+    ,1.1
+    ,0.8
+    ,1
 ];
 
 function printAllLists() {
