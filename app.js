b32bde83f5508f507fc6ba588c631429ff3238e8
Emmet <ekatzer@sjcawi.org>
2025-10-20 08:36:46 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,73 @@
+// Fruit Loops
+// The following lists are parallel lists
+// Use them to develop code mentioned in the comments below
+
+var fruits = [
+    "apple",
+    "banana",
+    "grapes",
+    "orange",
+    "dragonfruit",
+    "kiwi",
+    "starfruit"
+];
+
+var calories = [
+    95,
+    105,
+    35,
+    69,
+    61,
+    42,
+    28
+];
+
+var carbohydrates = [
+    25,
+    27,
+    9,
+    18,
+    15,
+    10,
+    6.1
+];
+
+var sugars = [
+    19,
+    14,
+    7.6,
+    12,
+    9,
+    6.2,
+    3.6
+];
+
+var fat = [
+    0.3,
+    0.4,
+    0.1,
+    0.2,
+    0.5,
+    0.4,
+    0.3
+];
+
+var protein = [
+    0.5,
+    0.4,
+    0.4,
+    1.3,
+    1.1,
+    0.8,
+    1
+];
+
+// Print a list of all fruits that have at least 50 calories
+
+// Print all fruits with a name greater than 7 letters
+
+// Print all fruits with less than 12 g of sugar
+
+// Print all fruits with more than 1 g of protein
+
+// Print all fruits with 80 or more calories and 20 g or more of carbs
