ac17923f67097fcf8d1eb3be5922e7646db49c26
Emmet <ekatzer@sjcawi.org>
2025-10-20 08:55:33 -0500

    More and better questions

--- app.js
+++ app.js
@@ -62,12 +62,94 @@ var protein = [
     1
 ];
 
+function printAllLists() {
+    console.log("fruits:");
+    console.log(fruits);
+    console.log("calories:");
+    console.log(calories);
+    console.log("sugars:");
+    console.log(sugars);
+    console.log("carbohydrates:");
+    console.log(carbohydrates);
+    console.log("fat:");
+    console.log(fat);
+    console.log("protein:");
+    console.log(protein);
+}
+
+printAllLists();
+
+//----------- Q1 -----------//
 // Print a list of all fruits that have at least 50 calories
+//for (___) {
+//
+//}
 
+//----------- Q2 -----------//
 // Print all fruits with a name greater than 7 letters
+//for (___) {
+//
+//}
 
+//----------- Q3 -----------//
 // Print all fruits with less than 12 g of sugar
+//for (___) {
+//
+//}
 
+//----------- Q4 -----------//
 // Print all fruits with more than 1 g of protein
+//for (___) {
+//
+//}
 
+//----------- Q5 -----------//
 // Print all fruits with 80 or more calories and 20 g or more of carbs
+//for (___) {
+//
+//}
+
+//----------- Q6 -----------//
+// Write a function that can add new fruits to ALL lists
+function addFruit(name, amountCalories, amountSugars, amountCarbohydrates, amountFat, amountProtein) {
+
+}
+
+//----------- Q7 -----------//
+// Write a function that can add new fruits to ALL lists
+// Add some new fruits to the lists
+// Use https://www.nutritionix.com/ for data
+//addFruit(___);
+//addFruit(___);
+//addFruit(___);
+
+console.log("\nI added some fruits\nNow the lists are:");
+printAllLists();
+
+//----------- Q8 -----------//
+// Write a function that can delete a fruit from ALL the lists by name
+
+function deleteFruit(name) {
+
+}
+
+//----------- Q9 -----------//
+// Delete some of the original fruits
+deleteFruit("apple");
+deleteFruit("starfruit");
+deleteFruit("orange");
+
+console.log("\nI deleted some fruits\nNow the lists are:");
+printAllLists();
+
+//----------- Q10 -----------//
+// Write a function that prints out the stats
+// of a fruit nicely
+function fruitStats(name) {
+
+}
+
+console.log("\nHere are some fruit stats:");
+fruitStats("dragonfruit");
+fruitStats("kiwi");
+fruitStats("grapes");
